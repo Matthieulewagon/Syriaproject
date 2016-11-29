44ffa134abc7a9aa6b790d9203a8ba34315fe4ac
Matthieu Lombard <matthieu.lombard@hotmail.be>
2016-11-29 17:34:03 +0100

    Add navbar and footer partials

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,8 +1,8 @@
 class Patient < ApplicationRecord
   belongs_to :user
   has_many :requests
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+    validates :last_name, presence: true
+validates :first_name, presence: true
   validates :address, presence: true
   validates :user_id, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Seed users
+user = User.create!(email: "matthieu@admin.com",
+                password: "foobar",
+                password_confirmation: "foobar")
+
+# Seed doctors
+10.times do |n|
+  user = User.create!(email: Faker::Internet.email,
+                password: "foobar",
+                password_confirmation: "foobar")
+
+  Doctor.create!(phone_number: Faker::PhoneNumber.cell_phone,
+                skype_username: Faker::Hipster.word,
+                first_name: Faker::Name.first_name,
+                last_name: Faker::Name.last_name,
+                address: Faker::Address.street_address,
+                user_id: user.id)
+end
+
+# Seed pharmacies
+10.times do |n|
+  user = User.create!(email: Faker::Internet.email,
+                password: "foobar",
+                password_confirmation: "foobar")
+
+  Pharmacy.create!(name: Faker::Hipster.word,
+                   phone_number: Faker::PhoneNumber.cell_phone,
+                   address: Faker::Address.street_address,
+                   user_id: user.id)
+end
