b36742b31818cd147517064e07a1424dafd1dfac
Matthieu Lombard <matthieu.lombard@hotmail.be>
2016-11-30 11:15:56 +0100

    index doctors

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'doctors/index'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # ressources :doctors
 end
