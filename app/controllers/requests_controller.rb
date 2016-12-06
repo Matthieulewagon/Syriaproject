02569f2517c182b93c6887acbfb9fb2e8046ee20
Matthieu Lombard <matthieu.lombard@hotmail.be>
2016-12-06 16:53:10 +0100

    sign up + request not finished

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,7 +1,19 @@
 class RequestsController < ApplicationController
-  def create
+  def new
+    @request = Request.new
   end
 
-  def new
+  def create
+     # @request = Request.new(request_params)
+     # @request.update_attributes(patient_id: params[:user_id])
+     @request = current_user.requests.create(request_params)
+     @request.save
+     redirect_to users_path
   end
+
+  def request_params
+      params.require(:request).permit(:description, :status,  :patient_id, :category_id, :doctor_id)
+  end
+
+
 end
