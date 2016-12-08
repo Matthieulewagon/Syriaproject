32ea2e0ac4701f72831a2122d35936b2cacc9de5
vivek koripalli <vivek.koripalli@gmail.com>
2016-12-08 11:32:06 +0100

    dd

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -61,11 +61,9 @@ ActiveAdmin.setup do |config|
   #
   # This setting changes the method which Active Admin calls
   # within the application controller.
-<<<<<<< HEAD
+
   config.authentication_method = :authenticate_user!
-=======
-  config.authentication_method = :authenticate_admin!
->>>>>>> 9289097781b006195344f1f3f12f115ac41efd84
+
 
   # == User Authorization
   #
