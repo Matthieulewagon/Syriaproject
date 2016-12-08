19bdc29ab6d9f8861ad29cf91ca457d650925a30
vivek koripalli <vivek.koripalli@gmail.com>
2016-12-08 13:08:06 +0100

    fixed-profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @hash = Gmaps4rails.build_markers(@user.address) do |user, marker|
+    @hash = Gmaps4rails.build_markers(@user) do |user, marker|
      marker.lat user.latitude
      marker.lng user.longitude
    end
