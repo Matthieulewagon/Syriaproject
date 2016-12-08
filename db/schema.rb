ff8c551ca2ff7f4a5abedf339eb81f20a78688e6
Matthieu Lombard <matthieu.lombard@hotmail.be>
2016-12-08 12:15:51 +0100

    fix conflit

--- db/schema.rb
+++ db/schema.rb
@@ -99,20 +99,12 @@ ActiveRecord::Schema.define(version: 20161207091020) do
     t.string   "facebook_picture_url"
     t.string   "token"
     t.datetime "token_expiry"
-<<<<<<< HEAD
-    t.float    "latitude"
-    t.float    "longitude"
-    t.string   "description"
-    t.string   "photo"
-    t.boolean  "admin",                  default: false, null: false
-=======
     t.boolean  "approved"
     t.string   "description"
     t.string   "photo"
     t.boolean  "admin",                  default: false, null: false
     t.float    "latitude"
     t.float    "longitude"
->>>>>>> 9289097781b006195344f1f3f12f115ac41efd84
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
