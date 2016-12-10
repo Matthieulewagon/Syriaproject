62e3005f7f38a58b5810faecb3f50d89458a38c3
Matthieu Lombard <matthieu.lombard@hotmail.be>
2016-12-10 12:55:21 +0100

    migrattion

--- db/migrate/20161208151633_add_approved_to_users.rb
+++ db/migrate/20161208151633_add_approved_to_users.rb
@@ -1,5 +1,5 @@
 class AddApprovedToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :approved, :boolean
+    add_column :users, :approved, :boolean, :default => false, :null => false
   end
 end
