124f7605c24c4804f27f901eea3a99e7b2b641bf
Matthieu Lombard <matthieu.lombard@hotmail.be>
2016-12-10 13:08:52 +0100

    migration approuve doctor

--- db/migrate/20161208151633_add_approved_to_users.rb
+++ db/migrate/20161208151633_add_approved_to_users.rb
@@ -1,5 +1,5 @@
 class AddApprovedToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :approved, :boolean, :default => false, :null => false
+    add_column role: 'doctor' , :approved, :boolean, :default => false, :null => false
   end
 end
