0a0b4fe36089fb0aff3d01cbfcb511d87381a005
Matthieu Lombard <matthieu.lombard@hotmail.be>
2016-12-10 14:35:47 +0100

    back to dmin to all user

--- db/migrate/20161208151633_add_approved_to_users.rb
+++ db/migrate/20161208151633_add_approved_to_users.rb
@@ -1,6 +1,6 @@
 class AddApprovedToUsers < ActiveRecord::Migration[5.0]
   def self.up
-    add_column role: 'doctor', :approved, :boolean, default: false
+    add_column :users, :approved, :boolean, default: false
     end
   end
 
