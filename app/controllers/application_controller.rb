20648b5e7c417e4ec8f3708531cb80fccc428980
Matthieu Lombard <matthieu.lombard@hotmail.be>
2016-12-14 16:25:41 +0100

    devise sanitize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :address, :phone_number, :skype_username, :role, :photo, :photo_cache, :document, :document_cache])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :address, :phone_number, :skype_username, :role, :photo, :photo_cache, :document, :document_cache, :gender])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username, :address, :phone_number, :skype_username, :role, :status, :photo, :photo_cache])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username, :address, :phone_number, :skype_username, :role, :status, :photo, :photo_cache, :gender])
   end
 
   def after_sign_in_path_for(user)
